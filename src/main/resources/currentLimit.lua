8d3f84bbb97f577bbd199460e8450c87491bf3dc
wanglongzhang <644527307@qq.com>
2020-06-12 10:51:47 +0800

    对redis的限流的有效期进行优化，赋予初始值的时候才设置有效期时间，如果达到限流大小超过限流大小10000，判断key的ttl是否出现问题，重新设置有效时间

--- src/main/resources/currentLimit.lua
+++ src/main/resources/currentLimit.lua
@@ -5,15 +5,29 @@ local key = KEYS[1]
 local limit = tonumber(ARGV[1])
 
 -- 获取当前流量大小
-local curentLimit = tonumber(redis.call('get', key) or "0")
+local currentLimit = tonumber(redis.call('get', key) or "0")
 
-if curentLimit + 1 > limit then
+if (limit + 10000 < currentLimit) then
+
+    -- 达到限流大小超过限流大小10000，可能是有效期出了问题
+    local ttlTime = redis.call('ttl',key)
+    if (ttlTime == -1) then
+        -- 如果发现有限期变成-1 设置有效期设置为2秒
+        redis.call("EXPIRE", key, 2)
+    end
+    return 0;
+elseif (currentLimit + 1 > limit) then
+
     -- 达到限流大小 返回
     return 0;
+elseif (currentLimit == 0) then
+    -- 没有达到阈值 value + 1 有效期设置为2秒
+    redis.call("INCRBY", key, 1)
+    redis.call("EXPIRE", key, 2)
+    return currentLimit + 1
 else
     -- 没有达到阈值 value + 1
     redis.call("INCRBY", key, 1)
-    redis.call("EXPIRE", key, 2)
-    return curentLimit + 1
+    return currentLimit + 1
 end
 
